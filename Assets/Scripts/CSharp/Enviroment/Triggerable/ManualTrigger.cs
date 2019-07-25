57c3491ff462a4c912021d71f25e23b50c910daf
Someoneawesome78 <Someoneawesome78@github.com>
2019-07-25 12:07:45 -0400

    WHY CANT I FREAKING COMMIT THIS

---
+++ Assets/Scripts/CSharp/Enviroment/Triggerable/ManualTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JLG.gift.cSharp.enviroment.triggerable {
+	public class ManualTrigger : Trigger {
+
+		public bool trigger = false;
+
+		private void Update() {
+			if (trigger) {
+				trigger = false;
+				triggerAll();
+			}
+		}
+
+	}
+}
---
+++ Assets/Scripts/CSharp/JLGscripts/Timeline/Behaviours/TimelineControlBehaviour.cs
@@ -0,0 +1,63 @@
+﻿using JLG.gift.cSharp.enviroment.triggerable;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+namespace JLG.gift.cSharp.jglScripts.timeline {
+	[Serializable]
+	public class TimelineControlBehaviour : PlayableBehaviour {
+
+		public static readonly double wiggle = 0.05;
+
+		public bool shouldFreeze;
+		public bool shouldJump;
+
+		[HideInInspector]
+		public TimelineControlClip clip;
+
+		//[NonSerialized]
+		//public TimelineClip clip;
+		//[HideInInspector]
+		//public TimelineControlClip clip;
+
+		public override void ProcessFrame(Playable playable, FrameData info, object playerData) {
+
+			var preScene = playerData as ITimeControlInteract;
+
+			if (preScene == null) {
+				Debug.LogError("null 1");
+				return;
+			}
+
+			if (clip == null) {
+				Debug.LogError("null 2");
+			}
+
+			PlayableDirector dir = preScene.getDirector();
+
+			if (dir == null) {
+				Debug.LogError("null 3");
+				return;
+			}
+
+			Debug.Log("current " + dir.time);
+			Debug.Log("end " + clip.end);
+
+			//test if on last frame
+			if (dir.time >= (clip.end - wiggle) && dir.time <= clip.end) {
+				Debug.LogWarning("Last");
+				//are we freeze
+				if (shouldFreeze && preScene.shouldContinueLoop()) {
+					dir.Pause();
+				} else if (shouldJump && preScene.shouldContinueLoop()) {
+					dir.time = clip.start;
+				}
+			}
+
+		}
+
+	}
+}
