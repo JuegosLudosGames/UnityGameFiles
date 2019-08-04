772aab69d12c7f07231dee3d33632dec535b7a56
Someoneawesome78 <Someoneawesome78@github.com>
2019-08-04 18:20:26 -0400

    Prepared SaveRevampt
    Added required classes in systemdata folder
    Added required dlls for SQLite

---
+++ Assets/Scripts/CSharp/SystemData/DataIO.cs
@@ -0,0 +1,24 @@
+﻿using JLG.gift.cSharp.background.input;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace JLG.gift.cSharp.SystemData {
+	[System.Serializable]
+	public class DataIO {
+
+		//video settings
+		public SerializableKeyInput SerializedkeyBindings;
+		public bool displayFPS;
+		public int targetFPS;
+
+		//audio settings
+		public int MVolume;
+		public int SFXVolume;
+		public int MuVolume;
+		public bool SFXMute;
+		public bool MuMute;
+
+	}
+}
---
+++ Assets/Scripts/CSharp/SystemData/PlayerSave.cs
@@ -0,0 +1,16 @@
+﻿using JLG.gift.cSharp.inventory;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JLG.gift.cSharp.SystemData {
+	[System.Serializable]
+	public class PlayerSave {
+		public SerializableInventory Inventory;
+		public float StrengthLevelIncrease;
+		public float SpellStrengthLevelIncrease;
+		public float MaxHealth;
+		public float MaxMana;
+		public int buildId;
+	}
+}
